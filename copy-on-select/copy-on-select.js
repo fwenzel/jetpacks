098e4f1ae3616e728136e9fc6b8a453f49d8734c
Frederic Wenzel <fwenzel@mozilla.com>
2009-11-12 18:29:05 -0800

    removing jbalogh's jetpacks :(

--- copy-on-select/copy-on-select.js
+++
@@ -1,7 +0,0 @@
-// Copy on select is awesome.
-jetpack.future.import("selection");
-jetpack.future.import("clipboard");
-
-jetpack.selection.onSelection(function(){
-    jetpack.clipboard.set(jetpack.selection.text);
-});
