3250ca14b0af6701bbc05aeace22ca7d0c57a566
Fred Wenzel <fwenzel@mozilla.com>
2009-11-25 11:14:21 +0100

    "assign to me" button in bugzilla

---
+++ bugzilla/assign-to-me/assign-to-me.js
@@ -0,0 +1,21 @@
+jetpack.future.import("pageMods");
+
+jetpack.pageMods.add(function(doc){
+    var $doc = $(doc);
+    var logout_re = /[\s\S]*Log.*out.*[\s]\s*(.*)\s/m,
+        logout_link = $doc.find('#footer #links-actions li:last'),
+        user_name = logout_link.text().replace(logout_re, '$1');
+    var assigned_to = $doc.find('#assigned_to');
+
+    // already the assignee?
+    if (assigned_to.val() == user_name) return;
+
+    // otherwise, make an assignee button
+    var button = $('<button>Assign to me</button>', doc);
+    button.click(function() {
+        assigned_to.val(user_name);
+        $doc.find('#commit_top').click();
+    });
+    $doc.find('#bz_assignee_edit_container').append(button);
+},
+['https://bugzilla.mozilla.org/show_bug.cgi*']);
